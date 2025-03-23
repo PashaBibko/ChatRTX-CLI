6da0bc7a4eeefcd50e41e7e61b822dae1ed02a70
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-03-23 15:58:23 +0000

    Organized files and their contents

--- Run.bat
+++ Run.bat
@@ -1,12 +1,15 @@
 @echo off
 
+REM Creates a folder to store the chats
+mkdir Chats
+
 REM Lable for start of loop
 :loop
 	REM Clears the screen for clearer output
 	cls
 
 	REM Runs the ChatRTX CLI
-	"C:\\Program Files\\NVIDIA Corporation\\ChatRTX\\env_nvd_rag\\Scripts\\python.exe" CLI.py
+	"C:\\Program Files\\NVIDIA Corporation\\ChatRTX\\env_nvd_rag\\Scripts\\python.exe" Main.py
 
 	REM Checks if it returned restart exit code
 	if %ERRORLEVEL% == 201 (
